3c6aa16381b0f6beb16ab4c4e5648fb592c36a31
Nicole Zhang <nicolexzha@gmail.com>
2022-07-21 11:29:39 -0700

    Finished music page

--- Shine/musicViewController.swift
+++ Shine/musicViewController.swift
@@ -72,11 +72,29 @@ class musicViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func harryButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/5r36AJ6VOJtp00oxSkBZ5h?si=5bfead6b2ae84c8e")!)
     }
     @IBAction func jojiButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/track/6xGruZOHLs39ZbVccQTuPZ?si=21a6c55a9c3b48df")!)
     }
-    
-
+    @IBAction func conanButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/5hIOd0FvjlgG4uLjXHkFWI?si=71f3d3e27795479b")!)
+    }
+    @IBAction func taylorButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/1NAmidJlEaVgA3MpcPFYGq?si=4a581bcef3a24f65")!)
+    }
+    @IBAction func frankButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/3mH6qwIy9crq0I9YQbOuDf?si=fKaW20N0R_C-rpBm4I8gOg")!)
+    }
+    @IBAction func oneButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/2sWX3HYnZjPZ9MrH6MFsBt?si=kxK9-0qDTiW0h1AL7zGlEw")!)
+    }
+    @IBAction func drakeButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/3cf4iSSKd8ffTncbtKljXw?si=2d3542904ec74d9d")!)
+    }
+    @IBAction func oliviaButton(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://open.spotify.com/album/6s84u2TUpR3wdUv4NgKA2j?si=776e2ec56c464a7f")!)
+    }
     /*
     // MARK: - Navigation
 
